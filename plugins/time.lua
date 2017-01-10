60c04e1432f602666b2f64da08c645f12c483df6
aliheydarian76 <aaa830400@gmail.com>
2017-01-10 00:02:16 -0800

    @aliheydarian76

---
+++ plugins/time.lua
@@ -0,0 +1,21 @@
+--[[
+#
+#    Time And Date
+#
+#    @WaderTGTeam
+# @WaderTGTeam
+#
+#
+]]
+
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '>>Time: '..jdat.FAtime..' \n>>Today: '..jdat.FAdate..'\n------------------\n>> '..jdat.ENtime..'\n>> '..jdat.ENdate.. '\n------------------\n@SoftTG'
+return text
+end
+return {
+  patterns = {"^[#/!]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+}
